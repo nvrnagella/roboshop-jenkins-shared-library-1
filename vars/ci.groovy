e9de9e84313f9fa3cb9e5e45f4ca77d43c81a385
IT1089778 <nvrnagella@gmail.com>
2023-04-22 12:08:45 +0530

    created global shared library

---
+++ vars/ci.groovy
@@ -0,0 +1,39 @@
+def call(){
+    pipeline{
+        agent{
+            label 'ansible'
+        }
+        stages{
+            stage('clean jenkins workspace'){
+                steps{
+                    cleanWs()
+                }
+            }
+            stage('compile/built'){
+                steps{
+                    echo 'compiling'
+                }
+            }
+            stage('unit test'){
+                steps{
+                    echo 'running test cases'
+                }
+            }
+            stage('code analysis'){
+                steps{
+                    echo 'scanning code in sonar qube'
+                }
+            }
+            stage('code to centralized place'){
+                steps{
+                    echo 'code to centralized place'
+                }
+            }
+        }
+        post{
+            always{
+                echo "sending an email"
+            }
+        }
+    }
+}
