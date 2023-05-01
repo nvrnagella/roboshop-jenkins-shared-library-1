2bfd46cf4a926e90de815cdc921950a471aa21dc
IT1089778 <nvrnagella@gmail.com>
2023-05-01 11:48:03 +0530

    created try catch block for exception handling

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,37 +1,41 @@
 def call(){
-    pipeline{
-        agent{
-            label 'ansible'
-        }
-        stages{
-            stage('compile/built'){
-                steps{
-                    script{
-                        common.compile()
-                    }
+    try{
+        pipeline{
+            agent{
+                label 'ansible'
+            }
+            stages{
+                stage('compile/built'){
+                    steps{
+                        script{
+                            common.compile()
+                        }
 
+                    }
+                }
+                stage('unit test'){
+                    steps{
+                        echo 'running test cases'
+                    }
+                }
+                stage('code analysis'){
+                    steps{
+                        echo 'scanning code in sonar qube'
+                    }
+                }
+                stage('code to centralized place'){
+                    steps{
+                        echo 'code to centralized place'
+                    }
                 }
             }
-            stage('unit test'){
-                steps{
-                    echo 'running test cases'
-                }
-            }
-            stage('code analysis'){
-                steps{
-                    echo 'scanning code in sonar qube'
-                }
-            }
-            stage('code to centralized place'){
-                steps{
-                    echo 'code to centralized place'
+            post{
+                always{
+                    echo "sending an email"
                 }
             }
         }
-        post{
-            always{
-                echo "sending an email"
-            }
-        }
+    }catch (){
+        email("failed")
     }
 }
--- vars/common.groovy
+++ vars/common.groovy
@@ -2,4 +2,15 @@ def compile(){
     if (app_lang == "nodejs"){
         sh 'npm install'
     }
+    if (app_lang == "maven"){
+        sh 'mvn compile'
+    }
+}
+def unittest(){
+    if (app_lang == "nodejs"){
+        sh 'npm test'
+    }
+}
+def email(email_note){
+    echo "sending an email"
 }
