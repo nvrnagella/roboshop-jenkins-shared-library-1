2bae0bbe9dc29a3fd7fa3d0be65df924dd863cb2
IT1089778 <nvrnagella@gmail.com>
2023-05-02 20:49:12 +0530

    making sonar scanner

--- vars/common.groovy
+++ vars/common.groovy
@@ -18,7 +18,9 @@ def unittest(){
     }
 }
 def qualitycheck(){
-    sh 'sonar-scanner -Dsonar.host.url=http://172.31.4.215:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.projectKey=cart'
+    sh 'USER=$(aws ssm get-parameters --names sonar.user --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+    sh 'PASS=$(aws ssm get-parameters --names sonar.pass --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+    sh "sonar-scanner -Dsonar.host.url=http://172.31.4.215:9000 -Dsonar.login=${USER} -Dsonar.password=${PASS} -Dsonar.projectKey=${component}"
 }
 def email(email_note){
     //mail bcc: '', body: "failure job name- ${JOB_BASE_NAME} \n failed job url - ${JOB_URL} ", cc: '', from: 'nvrnagella90@gmail.com', replyTo: '', subject: 'job failured', to: 'nvrnagella@gmail.com'
