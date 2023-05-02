ffceb49a16166eb78bbafc7e786a101ece807db8
IT1089778 <nvrnagella@gmail.com>
2023-05-02 12:38:07 +0530

    quality date

--- vars/common.groovy
+++ vars/common.groovy
@@ -8,7 +8,7 @@ def compile(){
 }
 def unittest(){
     if (app_lang == "nodejs"){
-        sh 'npm test' | echo
+        sh 'npm test' || echo
     }
     if (app_lang == "maven"){
         sh 'mvn test'
