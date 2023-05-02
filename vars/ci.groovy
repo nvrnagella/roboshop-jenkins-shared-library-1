d7f4d5d3240fcc76f7f77396f1d493b54742473e
IT1089778 <nvrnagella@gmail.com>
2023-05-02 21:01:12 +0530

    making sonar scanner

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -21,9 +21,13 @@ def call(){
                     }
                 }
                 stage('code analysis'){
+                    environment{
+                        USER='$(aws ssm get-parameters --names sonar.user --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+                        PASS='$(aws ssm get-parameters --names sonar.pass --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+                    }
                     steps{
                         script{
-                            common.qualitycheck()
+                            sh "sonar-scanner -Dsonar.host.url=http://172.31.4.215:9000 -Dsonar.login=${USER} -Dsonar.password=${PASS} -Dsonar.projectKey=${component}"
                         }
                     }
                 }
