c7c9f74acf36fa7091e47bf61394aad75890deda
IT1089778 <nvrnagella@gmail.com>
2023-05-02 12:36:21 +0530

    quality date

--- vars/common.groovy
+++ vars/common.groovy
@@ -17,6 +17,9 @@ def unittest(){
         sh 'python3 -m unittest'
     }
 }
+def qualitycheck(){
+    sh 'sonar-scanner -Dsonar.host.url=http://172.31.11.53:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.projectKey=cart'
+}
 def email(email_note){
     //mail bcc: '', body: "failure job name- ${JOB_BASE_NAME} \n failed job url - ${JOB_URL} ", cc: '', from: 'nvrnagella90@gmail.com', replyTo: '', subject: 'job failured', to: 'nvrnagella@gmail.com'
     println "sending failed job status to authorized persons"
