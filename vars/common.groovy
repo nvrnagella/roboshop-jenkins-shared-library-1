77c3a02323cc77aaf00d216fbe0ac3b4baa07d7a
IT1089778 <nvrnagella@gmail.com>
2023-05-02 20:54:48 +0530

    making sonar scanner

--- vars/common.groovy
+++ vars/common.groovy
@@ -18,8 +18,8 @@ def unittest(){
     }
 }
 def qualitycheck(){
-    sh 'USER=$(aws ssm get-parameters --names sonar.user --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
-    sh 'PASS=$(aws ssm get-parameters --names sonar.pass --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+    sh 'USER="$(aws ssm get-parameters --names sonar.user --with-decryption --query Parameters[0].Value | sed \'s/"//g\')"'
+    sh 'PASS="$(aws ssm get-parameters --names sonar.pass --with-decryption --query Parameters[0].Value | sed \'s/"//g\')"'
     sh "sonar-scanner -Dsonar.host.url=http://172.31.4.215:9000 -Dsonar.login=${USER} -Dsonar.password=${PASS} -Dsonar.projectKey=${component}"
 }
 def email(email_note){
