81b160d53fc621ff75b3e148dc97fbebb5889b30
IT1089778 <nvrnagella@gmail.com>
2023-05-03 18:39:35 +0530

    sonar java runnig

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -16,7 +16,7 @@ def call(){
                     SONAR_USER = sh (script: 'aws ssm get-parameters --region us-east-1 --name sonar.user --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
                     SONAR_PASSWORD = sh (script: 'aws ssm get-parameters --region us-east-1 --name sonar.pass --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
                     wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASSWORD}", var: 'SECRET']]]){
-                        sh "sonar-scanner -Dsonar.host.url=http://172.31.10.23:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASSWORD} -Dsonar.projectKey=${component} -Dsonar.java.binaries=./target"
+                        sh "sonar-scanner -Dsonar.host.url=http://172.31.10.23:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASSWORD} -Dsonar.projectKey=${component} ${SONAR_EXTRA_OPTS}"
                     }
                 }
 
