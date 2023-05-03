be0f526cb97ba2bc58b7c2eeddbaad1151e6eca3
IT1089778 <nvrnagella@gmail.com>
2023-05-03 17:28:24 +0530

    ci-declarative for reference

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,50 +1,24 @@
 def call(){
     try{
-        pipeline{
-            agent{
-                label 'ansible'
-            }
+        node('Build-In Node'){
             environment{
                 THE_CREDENTIALS=credentials('venkat-aws-cred')
             }
-            stages{
-                stage('compile/built'){
-                    steps{
-                        script{
-                            common.compile()
-                        }
-
-                    }
+            stage('compile/built'){
+                common.compile()
                 }
-                stage('unit test'){
-                    steps{
-                        script{
-                            common.unittest()
-                        }
-                    }
-                }
-                stage('Quality control'){
-                    steps{
-                        script{
-                           SONAR_USER='$(aws ssm get-parameters --region us-east-1 --name sonar.user --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
-                           SONAR_PASSWORD=sh (script: 'aws ssm get-parameters --region us-east-1 --name sonar.pass --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
-                           wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASSWORD}", var: 'SECRET']]]){
-                              sh "sonar-scanner -Dsonar.host.url=http://172.31.10.23:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASSWORD} -Dsonar.projectKey=${component}"
-                              }
-                        }
-
-                    }
-                }
-                stage('code to centralized place'){
-                    steps{
-                        echo 'code to centralized place'
-                    }
+            stage('unit test'){
+                common.unittest()
+            }
+            stage('Quality control'){
+                SONAR_USER='$(aws ssm get-parameters --region us-east-1 --name sonar.user --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+                SONAR_PASSWORD=sh (script: 'aws ssm get-parameters --region us-east-1 --name sonar.pass --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+                wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASSWORD}", var: 'SECRET']]]){
+                    sh "sonar-scanner -Dsonar.host.url=http://172.31.10.23:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASSWORD} -Dsonar.projectKey=${component}"
                 }
             }
-            post{
-                always{
-                    echo "this is post section"
-                }
+            stage('code to centralized place'){
+                echo 'code to centralized place'
             }
         }
     }catch(Exception e){
