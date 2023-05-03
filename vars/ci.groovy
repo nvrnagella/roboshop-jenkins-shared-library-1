94fb50d2cd66a0a32cb0720c4a6966951aee3cd1
IT1089778 <nvrnagella@gmail.com>
2023-05-03 17:10:30 +0530

    environment variables

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -27,7 +27,7 @@ def call(){
                     steps{
                         script{
                            SONAR_USER='$(aws ssm get-parameters --region us-east-1 --name sonar.user --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
-                           SONAR_PASSWORD='$(aws ssm get-parameters --region us-east-1 --name sonar.pass --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+                           SONAR_PASSWORD=sh (script: 'aws ssm get-parameters --region us-east-1 --name sonar.pass --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
                            wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASSWORD}", var: 'SECRET']]]){
                               sh "sonar-scanner -Dsonar.host.url=http://172.31.10.23:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASSWORD} -Dsonar.projectKey=${component}"
                               }
