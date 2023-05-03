9b6821363c766d7f2e9917c28bc239dbfe5aff70
IT1089778 <nvrnagella@gmail.com>
2023-05-03 17:48:11 +0530

    declarative converted to scripted pipeline

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -8,7 +8,7 @@ def call(){
                 common.unittest()
             }
             stage('Quality control'){
-                SONAR_USER='$(aws ssm get-parameters --region us-east-1 --name sonar.user --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+                SONAR_USER=sh (script: 'aws ssm get-parameters --region us-east-1 --name sonar.user --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
                 SONAR_PASSWORD=sh (script: 'aws ssm get-parameters --region us-east-1 --name sonar.pass --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
                 wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASSWORD}", var: 'SECRET']]]){
                     sh "sonar-scanner -Dsonar.host.url=http://172.31.10.23:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASSWORD} -Dsonar.projectKey=${component}"
