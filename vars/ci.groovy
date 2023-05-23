0d838c86bdf629ed83a19dbecc09c15140ef5064
IT1089778 <nvrnagella@gmail.com>
2023-05-23 15:14:04 +0530

    only push code if its tag created

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -2,6 +2,11 @@ def call(){
 //    if(!env.SONAR_EXTRA_OPTS){
 //        env.SONAR_EXTRA_OPTS = " "
 //    }
+    if(!env.TAG_NAME){
+        env.PUSH_CODE = "false"
+    }else{
+        env.PUSH_CODE = "true"
+    }
     try{
         node('workstation'){
             stage('clean workspace'){
