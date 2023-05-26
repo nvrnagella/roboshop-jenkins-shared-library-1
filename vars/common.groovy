f416ef0256b26d884eee87da24df033c7ea7ca72
IT1089778 <nvrnagella@gmail.com>
2023-05-27 02:51:53 +0530

    CREATED artifact push def function in common groovy file

--- vars/common.groovy
+++ vars/common.groovy
@@ -21,3 +21,12 @@ def email(email_note){
     //mail bcc: '', body: "failure job name- ${JOB_BASE_NAME} \n failed job url - ${JOB_URL} ", cc: '', from: 'nvrnagella90@gmail.com', replyTo: '', subject: 'job failured', to: 'nvrnagella@gmail.com'
     println "sending failed job status to authorized persons"
 }
+def artifactPush(){
+    withCredentials([aws(credentialsId: 'venkat-aws-cred', accessKeyVariable: 'AWS_ACCESS_KEY_ID', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]) {
+        NEXUS_USER = sh (script: 'aws ssm get-parameters --region us-east-1 --name nexus.user --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+        NEXUS_PASSWORD = sh (script: 'aws ssm get-parameters --region us-east-1 --name nexus.pass --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+        wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${NEUXS_PASSWORD}", var: 'SECRET']]]){
+            sh ""
+            }
+    }
+}
