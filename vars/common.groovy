1bc60fd59396c65db89c43f7ce0a0b0dfe57e5c6
IT1089778 <nvrnagella@gmail.com>
2023-05-27 09:35:22 +0530

    nexus ip address

--- vars/common.groovy
+++ vars/common.groovy
@@ -27,7 +27,7 @@ def artifactPush(){
         sh "zip -r ${component}-${TAG_NAME}.zip node_modules server.js VERSION ${extraFiles}"
     }
     withCredentials([aws(credentialsId: 'venkat-aws-cred', accessKeyVariable: 'AWS_ACCESS_KEY_ID', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]) {
-        NEXUS_USER = sh (script: 'aws ssm get-parameters --region us-east-1 --name nexus.user --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+        NEXUS_USER = $(aws ssm get-parameters --region us-east-1 --name nexus.user --with-decryption --query Parameters[0].Value | sed 's/"//g')
         NEXUS_PASSWORD = sh (script: 'aws ssm get-parameters --region us-east-1 --name nexus.pass --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
         wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${NEUXS_PASSWORD}", var: 'SECRET']]]){
             sh "curl -v -u ${NEXUS_USER}:${NEXUS_PASSWORD} --upload-file ${component}-${TAG_NAME}.zip http://172.31.10.126:8081/repository/${component}/${component}-${TAG_NAME}.zip"
