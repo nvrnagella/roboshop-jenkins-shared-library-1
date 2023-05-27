69941f5bdba400278c8e88ac17934b7dd871e3c5
IT1089778 <nvrnagella@gmail.com>
2023-05-27 10:18:55 +0530

    nexus ip address

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -40,8 +40,8 @@ def call(){
                         sh "zip -r ${component}-${TAG_NAME}.zip node_modules server.js VERSION ${extraFiles}"
                     }
                     withCredentials([aws(credentialsId: 'venkat-aws-cred', accessKeyVariable: 'AWS_ACCESS_KEY_ID', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]) {
-                        NEXUS_USER = $(aws ssm get-parameters --region us-east-1 --name sonar.user --with-decryption --query Parameters[0].Value | sed 's/"//g')
-                        NEXUS_PASSWORD = $(aws ssm get-parameters --region us-east-1 --name sonar.pass --with-decryption --query Parameters[0].Value | sed 's/"//g')
+                        NEXUS_USER = sh (script: 'aws ssm get-parameters --region us-east-1 --name sonar.user --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+                        NEXUS_PASSWORD = sh (script: 'aws ssm get-parameters --region us-east-1 --name sonar.pass --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
                         wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${NEXUS_PASSWORD}", var: 'SECRET']]]){
                             sh "curl -v -u ${NEXUS_USER}:${NEXUS_PASSWORD} --upload-file ${component}-${TAG_NAME}.zip http://172.31.10.126:8081/repository/${component}/${component}-${TAG_NAME}.zip"
                         }
