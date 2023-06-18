e2b70e9c6150f00e5a30dd4cc4895ef5ed801c29
IT1089778 <nvrnagella@gmail.com>
2023-06-18 13:39:03 +0530

    added terraform init stage

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -8,5 +8,12 @@ def call() {
         parameters {
             string(name: 'INFRA_ENV', defaultValue: '', description: 'Enter Env like dev or prod')
         }
+        stages {
+            stage('Terraform Init') {
+                steps {
+                    sh "terraform init -backend-config=env-${INFRA_ENV}/state.tfvars"
+                }
+            }
+        }
     }
 }
