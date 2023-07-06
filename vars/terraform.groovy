11da29c51e33f5433cae915e3bc06fc6a6cab1cf
IT1089778 <nvrnagella@gmail.com>
2023-07-06 21:18:38 +0530

    added choice to parameters

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -10,6 +10,7 @@ def call() {
         }
         parameters {
             string(name: 'INFRA_ENV', defaultValue: '', description: 'Enter Env like dev or prod')
+            choice(name: 'ACTION', choices: ['apply', 'destroy'], description: 'Action')
         }
         stages {
             stage('Terraform Init') {
@@ -19,7 +20,7 @@ def call() {
             }
             stage('Terraform apply') {
                 steps {
-                    sh "terraform apply -auto-approve -var-file=env-${INFRA_ENV}/main.tfvars"
+                    sh "terraform ${ACTION} -auto-approve -var-file=env-${INFRA_ENV}/main.tfvars"
                 }
             }
         }
